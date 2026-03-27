8333c9d040aeaa40ad32f8df2d1e40ecea0c6fdf
chanchal <garhewalchanchal@gmail.com>
2026-03-27 18:09:22 +0530

    Day-2: StudentResult

---
+++ Day-2/Day-2/src/StudentResult.java
@@ -0,0 +1,11 @@
+public class StudentResult {
+    public static void main (String[] args){
+        int marks =55;
+        if (marks>=40) {
+            System.out.println("Result:pass");
+        }else{
+            System.out.println("Result:Full");
+        }
+    }
+
+}
