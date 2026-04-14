fba2309a0cb5a272d40aa08a45e9666e0b84884e
chanchal <garhewalchanchal@gmail.com>
2026-04-14 21:57:21 +0530

    Added Day-1 src files properly

---
+++ Day-1/src/StructureDemo.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+import java.util.Scanner;
+public class StructureDemo {
+    public static void main (String[] args ){
+        System.out.println("Hello panda🐼🐼🐼🐼🐼");
+    }
+
+}
